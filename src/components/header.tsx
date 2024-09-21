cbaff4cbab8b6c255d5735be5052aea6592f5230
danielhep <dh@danielheppner.com>
2024-09-20 23:21:52 -0700

    add back button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,7 @@
+"use client";
+import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { FeedAndDate } from "~/data/feeds";
 
 export default function Header({
@@ -8,10 +11,12 @@ export default function Header({
   firstFeed?: FeedAndDate;
   secondFeed?: FeedAndDate;
 }) {
+  const pathname = usePathname();
   return (
     <header className="w-full items-center justify-between bg-purple-900 px-4 shadow-sm md:flex md:h-16">
-      <Link href="/" className="text-xl font-bold text-white md:text-4xl">
-        <h1>What the Bus?</h1>
+      <Link href="/" className="text-xl font-bold text-white md:text-4xl flex gap-3 items-center">
+        {pathname !== "/" && <ArrowLeft size={24} strokeWidth={3}/>}
+        <h1>Service Change Analyzer</h1>
       </Link>
     </header>
   );
