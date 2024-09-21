6548abcebfdc5ad2f6efd72d2f0b3b3a91d17028
danielhep <dh@danielheppner.com>
2024-09-20 22:26:32 -0700

    update language

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,25 +17,26 @@ export default function Home() {
   const [firstFeed, setFirstFeed] = useState<FeedAndDate | null>(null);
   const [secondFeed, setSecondFeed] = useState<FeedAndDate | null>(null);
   return (
-    <div className="m-auto mt-4 grid max-w-3xl grid-cols-1 space-y-4 my-4">
-      <h1 className="text-4xl font-bold">
+    <div className="m-auto my-4 mt-4 grid max-w-3xl grid-cols-1 space-y-4">
+      {/* <h1 className="text-4xl font-bold">
         What is <span className="text-purple-500">What the Bus?</span>
-      </h1>
-      <p className="text-xl">
-        Analyze transit service or compare two feeds to see how they have
-        changed over time.
+      </h1> */}
+      <p>
+        Compare two transit feeds or different dates in the same feed to see how
+        service has changed across a service change.
       </p>
       <p>
-        Begin with a preset that corresponds to the most recent/upcoming service
-        change or manually select your inputs below.
+        Use a helpful preset to quickly see current service changes for various
+        agencies, or manually select your inputs below.
       </p>
       <Card>
         <CardHeader>
           <CardTitle>Comparison Presets</CardTitle>
           <CardDescription>
             These presets are based on the most recent/upcoming service change
-            for the given agency. They will select service dates before and after
-            the change from the corresponding feed, and enter compare mode.
+            for the given agency. They will select service dates before and
+            after the change from the corresponding feed, and enter compare
+            mode.
           </CardDescription>
         </CardHeader>
         <CardContent>
@@ -46,32 +47,24 @@ export default function Home() {
         <CardHeader>
           <CardTitle>Manual Feed Selection</CardTitle>
           <CardDescription>
-            Manually select a feed and date to analyze or compare.
+            Select two feeds and dates to compare.
           </CardDescription>
         </CardHeader>
         <CardContent className="flex flex-col space-y-4">
           <FeedSelector onSelectionChange={setFirstFeed} />
-          <h4 className="text-sm text-muted-foreground">
-            Select second feed and enable comparison mode:
-          </h4>
+          <h4 className="text-sm text-muted-foreground">compare with:</h4>
           <FeedSelector onSelectionChange={setSecondFeed} />
-          {!secondFeed ? (
-            <Button disabled={!firstFeed} aria-disabled={!firstFeed}>
-              Analyze single feed
-            </Button>
-          ) : (
-            <Button
-              disabled={!firstFeed || !secondFeed}
-              aria-disabled={!firstFeed || !secondFeed}
-              asChild
+          <Button
+            disabled={!firstFeed || !secondFeed}
+            aria-disabled={!firstFeed || !secondFeed}
+            asChild
+          >
+            <Link
+              href={`/${firstFeed?.identifier}/compareTo/${secondFeed?.identifier}`}
             >
-              <Link
-                href={`/${firstFeed?.identifier}/compareTo/${secondFeed?.identifier}`}
-              >
-                Compare these feeds
-              </Link>
-            </Button>
-          )}
+              Compare these feeds
+            </Link>
+          </Button>
         </CardContent>
       </Card>
     </div>
